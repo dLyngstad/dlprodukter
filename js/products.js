b4d7845671f8538c0c625c03d810bfd56ade90e7
dLyngstad <ravendlyngstad@gmail.com>
2025-08-26 18:36:36 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,7 @@
 const products = [
     {
         id: 'prod_001',
+        ecwidId: 'PASTE_ID_HERE', // <-- Get ID for Classic Wooden Chair
         name: 'Classic Wooden Chair',
         price: '999.00',
         image: 'https://via.placeholder.com/150', // Placeholder image URL
@@ -8,6 +9,7 @@ const products = [
     },
     {
         id: 'prod_002',
+        ecwidId: 'PASTE_ID_HERE', // <-- Get ID for Modern Table Lamp
         name: 'Modern Table Lamp',
         price: '450.00',
         image: 'https://via.placeholder.com/150',
@@ -15,6 +17,7 @@ const products = [
     },
     {
         id: 'prod_003',
+        ecwidId: 'PASTE_ID_HERE', // <-- Get ID for Minimalist Bookshelf
         name: 'Minimalist Bookshelf',
         price: '1499.00',
         image: 'https://via.placeholder.com/150',
@@ -22,6 +25,7 @@ const products = [
     },
     {
         id: 'prod_004',
+        ecwidId: 'PASTE_ID_HERE', // <-- Get ID for Cozy Wool Blanket
         name: 'Cozy Wool Blanket',
         price: '799.00',
         image: 'https://via.placeholder.com/150',
