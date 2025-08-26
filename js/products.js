055e115060b9640a88e145f15217d54191cb9bf9
dLyngstad <ravendlyngstad@gmail.com>
2025-08-26 20:59:17 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,7 +6,7 @@ const products = [
         ecwidId: '123196506', // <-- Bytt ut med din ID fra Ecwid
         name: 'Karabin krok',
         price: '5.00',
-        image: 'bilder/karabinkrok.jpg', // <-- Bytt ut med din bildesti
+        image: 'bilder/karabinkrok.png', // <-- Bytt ut med din bildesti
         description: 'En solid karabinkrok i stål. Perfekt for nøkler eller utstyr.' // <-- Bytt ut med din beskrivelse
     }
 ];
