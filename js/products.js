309c9eff6ec9bd2366fd4b55dc51cf95134208dd
dLyngstad <ravendlyngstad@gmail.com>
2025-08-26 23:07:18 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -8,6 +8,15 @@ const products = [
         price: '5.00',
         image: 'Bilder/kar2.png',
         description: 'En solid karabinkrok i stål. Perfekt for nøkler eller utstyr.'
+    },
+ {
+        id: 'prod_002',
+        ecwidId: '777573842', // <-- DETTE ER DEN KORREKTE ID-en fra bildet ditt
+        name: 'Vaske Sett',
+        price: '80',
+        image: 'Bilder/kar2.png',
+        description: 'Et vaskesett som er perfekt til rengjøring'
     }
+
     // ... legg til flere produkter her med deres unike Produkt ID-er
 ];
