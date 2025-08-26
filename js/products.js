a1d64d93dfc6108c7b91c8cecafbbded33e84915
dLyngstad <ravendlyngstad@gmail.com>
2025-08-26 19:49:48 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,29 +6,5 @@ const products = [
         price: '999.00',
         image: 'https://via.placeholder.com/150',
         description: '--------------------'
-    },
-    {
-        id: 'prod_002',
-        ecwidId: 'YOUR_ECWID_ID_HERE', // <-- Find and paste the ID for this product
-        name: 'Modern Table Lamp',
-        price: '450.00',
-        image: 'https://via.placeholder.com/150',
-        description: '--------------------'
-    },
-    {
-        id: 'prod_003',
-        ecwidId: 'YOUR_ECWID_ID_HERE', // <-- Find and paste the ID for this product
-        name: 'Minimalist Bookshelf',
-        price: '1499.00',
-        image: 'https://via.placeholder.com/150',
-        description: '--------------------'
-    },
-    {
-        id: 'prod_004',
-        ecwidId: 'YOUR_ECWID_ID_HERE', // <-- Find and paste the ID for this product
-        name: 'Cozy Wool Blanket',
-        price: '799.00',
-        image: 'https://via.placeholder.com/150',
-        description: '--------------------'
     }
 ];
