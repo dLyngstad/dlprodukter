4eed3f61022541e3f73c904d847ce533c86ee3a3
dLyngstad <ravendlyngstad@gmail.com>
2025-08-26 22:29:42 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -3,10 +3,11 @@
 const products = [
     {
         id: 'prod_001',
-        ecwidId: '777515903', // <-- Bytt ut med din ID fra Ecwid
+        ecwidId: '777515903', // <-- DETTE ER DEN KORREKTE ID-en fra bildet ditt
         name: 'Karabin krok',
         price: '5.00',
-        image: 'Bilder/kar2.png', // <-- Bytt ut med din bildesti
-        description: 'En solid karabinkrok i stål. Perfekt for nøkler eller utstyr.' // <-- Bytt ut med din beskrivelse
+        image: 'Bilder/kar2.png',
+        description: 'En solid karabinkrok i stål. Perfekt for nøkler eller utstyr.'
     }
+    // ... legg til flere produkter her med deres unike Produkt ID-er
 ];
