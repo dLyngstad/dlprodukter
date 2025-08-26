53e6dd7360f08fe0272e3dc279b2e4ff23a47e4d
dLyngstad <ravendlyngstad@gmail.com>
2025-08-26 22:13:40 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -3,7 +3,7 @@
 const products = [
     {
         id: 'prod_001',
-        ecwidId: '123196506', // <-- Bytt ut med din ID fra Ecwid
+        ecwidId: '777515903', // <-- Bytt ut med din ID fra Ecwid
         name: 'Karabin krok',
         price: '5.00',
         image: 'Bilder/kar2.png', // <-- Bytt ut med din bildesti
