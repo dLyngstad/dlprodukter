73936041c70524bccc3e65ca21e92537484e13dc
dLyngstad <ravendlyngstad@gmail.com>
2025-08-27 16:42:27 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,47 +1,37 @@
-// Fil: products.js
-const products = [
-    {
-        id: 'prod_001',
-        ecwidId: '777515903',
-        name: 'Karabin krok',
-        price: '5.00',
-        images: [
-            'Bilder/karabinkrok_1.png', 
-            'Bilder/karabinkrok_2.png'
-        ],
-        description: 'En solid karabinkrok i stål.'
-    },
-    {
-        id: 'prod_002',
-        ecwidId: '777573842',
-        name: 'Vaske Sett',
-        price: '80',
-        images: [
-            'Bilder/vaskesett_1.png', 
-            'Bilder/vaskesett_2.png'
-        ],
-        description: 'Et vaskesett som er perfekt til rengjøring.'
-    },
-    {
-        id: 'prod_003',
-        ecwidId: '777591031',
-        name: 'Egge Form',
-        price: '80',
-        images: [
-            'Bilder/eggeform_1.png',
-            'Bilder/eggeform_2.png'
-        ],
-        description: 'Til å forme egg i stekepanne.'
-    },
-    {
-        id: 'prod_004',
-        ecwidId: '777743139',
-        name: 'Glass',
-        price: '160',
-        images: [
-            'Bilder/lupe_1.png',
-            'Bilder/lupe_2.png'
-        ],
-        description: 'Til a se maur eller andre insekter.'
-    },
+
+// Fil: products.js 
+
+const products = [ 
+    { 
+        id: 'prod_001', 
+        ecwidId: '777515903', 
+        name: 'Karabin krok', 
+        price: '5.00', 
+        image: 'Bilder/kar2.png', 
+        description: 'En solid karabinkrok i stål. Perfekt for nøkler eller utstyr.' 
+    }, 
+    { 
+        id: 'prod_002', 
+        ecwidId: '777573842', 
+        name: 'Vaske Sett', 
+        price: '80', 
+        image: 'Bilder/vaskesett.png', 
+        description: 'Et vaskesett som er perfekt til rengjøring' 
+    }, // <-- DU MANGLER ET KOMMA HER. JEG HAR LAGT DET TIL. 
+    { 
+        id: 'prod_003', 
+        ecwidId: '777591031', 
+        name: 'Egge Form', 
+        price: '80', 
+        image: 'Bilder/eggeform.png', 
+        description: 'Til å forme egg i stekepanne' 
+    }, // <-- DU MANGLER ET KOMMA HER. JEG HAR LAGT DET TIL. 
+    { 
+        id: 'prod_004', 
+        ecwidId: '777743139', 
+        name: 'Glass', 
+        price: '160', 
+        image: 'Bilder/lupe.png', 
+        description: 'Til a se maur eller andre insekter' 
+    }, 
 ];
