2c31606c207eaa1f5dec80687f888829a826f3ae
dLyngstad <ravendlyngstad@gmail.com>
2025-08-27 23:20:21 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,29 +1,47 @@
 // Fil: products.js
-
 const products = [
     {
         id: 'prod_001',
         ecwidId: '777515903',
         name: 'Karabin krok',
         price: '5.00',
-        image: 'Bilder/kar2.png',
-        description: 'En solid karabinkrok i stål. Perfekt for nøkler eller utstyr.'
+        images: [
+            'Bilder/karabinkrok_1.png', 
+            'Bilder/karabinkrok_2.png'
+        ],
+        description: 'En solid karabinkrok i stål.'
     },
     {
         id: 'prod_002',
         ecwidId: '777573842',
         name: 'Vaske Sett',
         price: '80',
-        image: 'Bilder/vaskesett.png',
-        description: 'Et vaskesett som er perfekt til rengjøring'
-    }, // <-- DU MANGLER ET KOMMA HER. JEG HAR LAGT DET TIL.
+        images: [
+            'Bilder/vaskesett_1.png', 
+            'Bilder/vaskesett_2.png'
+        ],
+        description: 'Et vaskesett som er perfekt til rengjøring.'
+    },
     {
         id: 'prod_003',
         ecwidId: '777591031',
         name: 'Egge Form',
         price: '80',
-        image: 'Bilder/eggeform.png',
-        description: 'Til å forme eggene dine'
-    } // It's good practice to have a comma here too, but it's not required for the last item.
-
+        images: [
+            'Bilder/eggeform_1.png',
+            'Bilder/eggeform_2.png'
+        ],
+        description: 'Til å forme egg i stekepanne.'
+    },
+    {
+        id: 'prod_004',
+        ecwidId: '777743139',
+        name: 'Glass',
+        price: '160',
+        images: [
+            'Bilder/lupe_1.png',
+            'Bilder/lupe_2.png'
+        ],
+        description: 'Til a se maur eller andre insekter.'
+    },
 ];
