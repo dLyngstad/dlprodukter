13a4a9e749c31583551dc24cc1545a7fc6515f98
dLyngstad <ravendlyngstad@gmail.com>
2025-09-13 00:00:13 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -5,7 +5,7 @@ const products = [
         ecwidId: '778175166',
         name: 'Elektrisk Forsegler',
         price: '149',
-        images: ['Bilder/bagsealerimages/bagsealerA.png', 'Bilder/bagsealerimages/bagsealerB.png','Bilder/bagsealerimages/bagsealerC.png','Bilder/bagsealerimages/bagsealerD.png','Bilder/bagsealerimages/bagsealerE.png','Bilder/bagsealerimages/bagsealerF.png','Bilder/bagsealerimages/bagsealerA.png'],
+        images: ['Bilder/bagsealerimages/bagsealerA.png', 'Bilder/bagsealerimages/bagsealerB.png','Bilder/bagsealerimages/bagsealerC.png','Bilder/bagsealerimages/bagsealerD.png','Bilder/bagsealerimages/bagsealerE.png','Bilder/bagsealerimages/bagsealerF.png','Bilder/bagsealerimages/bagsealerG.png'],
         description: 'Pose forsegler! Forsegler alt du vil. .',
         moreInfo: 'Følger med ladekabel og instruksjoner.<br>- Implementert magnet for å feste på kjøleskap. <br> - Implementert kniv for å skjøre opp poser.'
     },
